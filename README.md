378f2138386bdd41b9b5fc3edab44a8c8520def5
Helene Selvig <helenkse@stud.ntnu.no>
2024-10-15 01:07:12 +0200

    feat: :sparkles: clickable box components and style changes
    added components category, slot, cover and challenge. Added functionality to slots, and editable text for category titles and category slots

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # React + TypeScript + Vite
 
+- root/App
+  - Card
+    - h1
+    - body
+  - div
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+interface CardProps {
+  text: string
+  age: number
+}
+function Card(props: CardProps) {
+  return (
+    <>
+      <h1>{props.text}</h1>
+      <body>
+        {props.age}
+      </body>
+    </>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Card text='hello' age={2} />
+
+
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
