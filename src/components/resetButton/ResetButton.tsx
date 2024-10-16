cc8b447417958fdcddb451b4e1213914cff5b93e
Tobias Hegge <tobiashegge05@gmail.com>
2024-10-16 21:18:13 +0200

    feat: :lipstick: Improved style of button
    Fixed issue where button is off screen. Made button presses more visible by changing scale and color. Made a function for when button is presse, but the function is empty and does not do anything yet.

--- src/components/resetButton/ResetButton.tsx
+++ src/components/resetButton/ResetButton.tsx
@@ -1,9 +1,19 @@
+import Slot from '../slot/Slot'
 import './ResetButton.css'
 
 export default function ResetButton() {
+
+    const reset = () => {
+
+    }
+
     return (
         <>
-            <button type='button' className='resetButton-button'>RESET</button>
+            <button type='button'
+            className='resetButton-button'
+            onClick={reset}>
+                RESET
+            </button>
         </>
     )
 }
