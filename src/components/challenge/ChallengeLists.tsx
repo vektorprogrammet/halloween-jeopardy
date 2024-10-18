3660566d060bae63d3b7ad14f6709fc3a79e5f78
Tobias Hegge <tobiaheg@stud.ntnu.no>
2024-10-18 12:11:31 +0200

    refactor: :recycle: Moved challenge lists to a separate file

---
+++ src/components/challenge/ChallengeLists.tsx
@@ -0,0 +1,41 @@
+export const heksegryta_challenges: string[] = [
+    "Ta 1 slurk",
+    "Alle på laget danser samba",
+    "Chug glasset",
+    "Løp 2 mil, ta 100 burpees og skap fred i verden"
+];
+
+export const hahahalloween_challenges: string[] = [
+    "Ta 2 slurker",
+    "Alle på laget danser samba",
+    "Chug glasset",
+    "Løp 2 mil, ta 100 burpees og skap fred i verden"
+];
+
+export const murdermystery_challenges: string[] = [
+    "Ta 3 slurker",
+    "Alle på laget danser samba",
+    "Chug glasset",
+    "Løp 2 mil, ta 100 burpees og skap fred i verden"
+];
+
+export const trick_or_treat_challenges: string[] = [
+    "Ta 4 slurker",
+    "Alle på laget danser samba",
+    "Chug glasset",
+    "Løp 2 mil, ta 100 burpees og skap fred i verden"
+];
+
+export const godteriskala_challenges: string[] = [
+    "Ta 5 slurker",
+    "Alle på laget danser samba",
+    "Chug glasset",
+    "Løp 2 mil, ta 100 burpees og skap fred i verden"
+];
+
+export const kostymefest_challenges: string[] = [
+    "Ta 6 slurker",
+    "Alle på laget danser samba",
+    "Chug glasset",
+    "Løp 2 mil, ta 100 burpees og skap fred i verden"
+];
